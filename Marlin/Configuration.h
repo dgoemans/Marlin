94a35840101258bb8a8be271393763db22bd2de7
David Goemans <dgoemans@gmail.com>
2025-04-07 21:06:35 +0200

    Adjust printable area dimensions to 150mm for X and Y bed sizes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1754,8 +1754,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 180
-#define Y_BED_SIZE 180
+#define X_BED_SIZE 150
+#define Y_BED_SIZE 150
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
